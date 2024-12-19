894d1812cd21cc6fbb91c671f9a1dcfd6fafa713
Jatin <jatin.singhal@metaschool.so>
2024-12-19 10:22:06 +0530

    Update Step1.jsx

--- src/Components/Step1/Step1.jsx
+++ src/Components/Step1/Step1.jsx
@@ -317,13 +317,14 @@ const Step1 = ({ sharedState, updateSharedState, onNext }) => {
           }}
         className="w-full py-2.5 px-2.5 text-sm my-2.5 border border-gray-300 rounded bg-gray-100 text-black transition-all duration-200 focus:border-yellow-400 focus:outline-none appearance-none cursor-pointer bg-[url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23000000%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E')] bg-no-repeat bg-[right_10px_center] bg-[length:12px]"
       >
-        <option value="ETH">ETH</option>
-        <option value="ETH">Sepolia ETH</option>
+       <option value="ETH">Sepolia ETH</option>
+       <option value="META">Sepoila META</option>
+        {/* <option value="ETH">ETH</option>
         <option value="USDT">USDT</option>
         <option value="USDC">USDC</option>
         <option value="WETH">WETH </option>
-        <option value="DAI">DAI </option>
-        <option value="META">Sepoila META</option>
+        <option value="DAI">DAI </option> */}
+        
         <option value="CUSTOM">Custom Token</option>
       </select>
 
